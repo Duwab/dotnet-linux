a2f2595a833d834b31be29be4f7fdcdfb8e39163
Antoine Dubois <antoine.dubois@commandersact.com>
2021-12-19 12:15:54 +0100

    Initialize project

---
+++ README.md
@@ -0,0 +1,35 @@
+# Working with dotnet on Linux
+
+## Initialiser le projet
+
+```
+# initiate project
+dotnet new sln
+dotnet new console
+dotnet run
+
+# add dependencies
+dotnet add package WorkflowCore
+```
+
+Dans le container, on peut faire toutes les manips avec la version de dotnet 3.x
+
+> **Note:** Pour le container, aller chercher le container [`dotnet/sdk`](https://hub.docker.com/_/microsoft-dotnet-sdk/) qui match la version pour l'os et Dotnet cible
+
+Pour que l'éditeur fonctionne:
+* installer la version de dotnet SDK cible
+* ajouter le plugin c# le mieux maintenu de l'éditeur
+
+Pour faire fonctionner le debugger
+*Tuto debugger dotnet vscode*
+
+## Sample
+
+Ici, on fait un exemple avec le package WorkflowCore.
+
+```
+cd /app/console
+dotnet run
+```
+
+> **Note:** il ne semble pas falloir réaliser de "`npm install`" avec la [commande `dotnet`](https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet). Le run s'en charge ?
---
+++ app/console/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+using WorkflowCore;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pour la config, je copie colle ce qui était dans ce sample (la base pour les tests) https://github.com/danielgerlag/workflow-core/blob/06470ce83446a970ff95f2f070793cefa7f6e5cc/test/WorkflowCore.Testing/WorkflowCore.Testing.csproj
+            Console.WriteLine("Start!");
+            IServiceProvider serviceProvider = ConfigureServices();
+            var host = serviceProvider.GetService<IWorkflowHost>();
+            host.RegisterWorkflow<HelloWorldWorkflow>();
+            host.Start();
+            host.StartWorkflow("HelloWorld", 1, null);
+            Console.ReadLine(); // wait for async workflow to be over
+            host.Stop();
+            Console.WriteLine("End!");
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            //setup dependency injection
+            IServiceCollection services = new ServiceCollection();
+            services.AddLogging();
+            services.AddWorkflow();
+            var serviceProvider = services.BuildServiceProvider();
+
+            return serviceProvider;
+        }
+    }
+
+    public class HelloWorld : StepBody
+    {
+        public override ExecutionResult Run(IStepExecutionContext context)
+        {
+            Console.WriteLine("Hello world");
+            return ExecutionResult.Next();
+        }
+    }
+
+    public class GoodbyeWorld : StepBody
+    {
+        public override ExecutionResult Run(IStepExecutionContext context)
+        {
+            Console.WriteLine("Goodbye world");
+            Console.WriteLine("(type anything to quit)");
+            return ExecutionResult.Next();
+        }
+    }
+
+    public class HelloWorldWorkflow : IWorkflow
+    {
+        public string Id => "HelloWorld";
+        public int Version => 1;
+
+        public void Build(IWorkflowBuilder<object> builder)
+        {
+            builder
+                .StartWith<HelloWorld>()
+                .Then<GoodbyeWorld>();
+        }
+    }
+
+}
